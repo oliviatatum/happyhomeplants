d9dd9ff1e8d93906464dc67497852ee3e4256859
oliviatatum <olivia.tatum1@hotmail.com>
2020-12-29 22:56:17 +0000

    trying to fix a bug with noimage.png

--- products/models.py
+++ products/models.py
@@ -27,6 +27,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     description = models.TextField()
     image = models.ImageField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
     height = models.CharField(max_length=4, null=True, blank=True)
 
     def __str__(self):
