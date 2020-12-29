fa4a2e7093b5733e0b305652e65a3bdd74e70d8b
oliviatatum <olivia.tatum1@hotmail.com>
2020-12-29 21:05:07 +0000

    customised admin

---
+++ products/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    alternate_name = models.CharField(max_length=254, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+    height = models.CharField(max_length=4, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
