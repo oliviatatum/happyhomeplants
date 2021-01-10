d526429f6c251e7403696e85c4d2c930de748c22
oliviatatum <olivia.tatum1@hotmail.com>
2021-01-10 12:39:38 +0000

    added shopping basket preview

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from products.models import Product
 
 # Create your views here.
 
@@ -11,6 +14,7 @@ def view_basket(request):
 def add_to_basket(request, item_id):
     """ add a quantity of the specified product to the shopping basket"""
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
@@ -19,6 +23,8 @@ def add_to_basket(request, item_id):
         basket[item_id] += quantity
     else:
         basket[item_id] = quantity
+        messages.success(
+            request, f'{quantity} {product.friendly_name}(s) successfully added to your basket!')
 
     request.session['basket'] = basket
     return redirect(redirect_url)
