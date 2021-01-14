697e00c5ee5dd6e273876168135067207fc49e4c
oliviatatum <olivia.tatum1@hotmail.com>
2021-01-14 03:42:22 +0000

    added confirmation email functionality

--- happyhomeplants/settings.py
+++ happyhomeplants/settings.py
@@ -176,4 +176,4 @@ STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET')
-APPEND_SLASH = False
+DEFAULT_FROM_EMAIL = 'happyhomeplants@happyhomeplants.com'
