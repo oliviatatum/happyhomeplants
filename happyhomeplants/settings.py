d9410f6d9937168e991d57bd6137d7a30b6056b1
oliviatatum <olivia.tatum1@hotmail.com>
2021-01-14 16:07:04 +0000

    deployment

--- happyhomeplants/settings.py
+++ happyhomeplants/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '(fo1$-!l3w*dl&g4druue^u#h7dzl!e(^jo8$kpzy4v00(x!3w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['happyhomeplants.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -121,12 +121,17 @@ WSGI_APPLICATION = 'happyhomeplants.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
